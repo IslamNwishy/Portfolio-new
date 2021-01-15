63b04f4e034b81088f86ff4d1928b527b9da8962
IslamNwishy <islamosama@aucegypt.edu>
2021-01-15 20:41:14 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*------------------
+        Preloader
+    --------------------*/
+$(window).on("load", function () {
+  $(".loader").fadeOut();
+  $("#preloader").delay(200).fadeOut("slow");
+});
