9c042661ae5832141855072c58ecc9564c23bab1
IslamNwishy <islamosama@aucegypt.edu>
2021-01-16 22:27:28 +0200

    about me added

--- js/main.js
+++ js/main.js
@@ -5,3 +5,60 @@ $(window).on("load", function () {
   $(".loader").fadeOut();
   $("#preloader").delay(200).fadeOut("slow");
 });
+
+/*Sticky navbar*/
+window.addEventListener("scroll", function () {
+  var nav = $("#nav");
+  var preH = $("#anchor").offset().top;
+
+  if (window.scrollY >= preH - 100) {
+    nav.addClass("nav-outer-sticky");
+  } else {
+    nav.removeClass("nav-outer-sticky");
+  }
+});
+
+/*------------------
+        TimeLine
+    --------------------*/
+(function ($) {
+  $.fn.timeline = function () {
+    var selectors = {
+      id: $(this),
+      item: $(this).find(".timeline-item"),
+      activeClass: "timeline-item--active",
+      img: ".timeline__img",
+    };
+    selectors.item.eq(0).addClass(selectors.activeClass);
+    selectors.id.css(
+      "background-image",
+      "url(" + selectors.item.first().find(selectors.img).attr("src") + ")"
+    );
+    var itemLength = selectors.item.length;
+    $(window).scroll(function () {
+      var max, min;
+      var pos = $(this).scrollTop();
+      selectors.item.each(function (i) {
+        min = $(this).offset().top - 100;
+        max = $(this).height() + $(this).offset().top;
+        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+          selectors.item.removeClass(selectors.activeClass);
+          selectors.id.css(
+            "background-image",
+            "url(" + selectors.item.last().find(selectors.img).attr("src") + ")"
+          );
+          selectors.item.last().addClass(selectors.activeClass);
+        } else if (pos <= max - 40 && pos >= min) {
+          selectors.id.css(
+            "background-image",
+            "url(" + $(this).find(selectors.img).attr("src") + ")"
+          );
+          selectors.item.removeClass(selectors.activeClass);
+          $(this).addClass(selectors.activeClass);
+        }
+      });
+    });
+  };
+})(jQuery);
+
+$("#timeline-1").timeline();
