9849386d64389c6d714493ba7972095c36760acd
IslamNwishy <islamosama@aucegypt.edu>
2021-01-18 03:03:37 +0200

    removed some unnecessary code

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ $(window).on("load", function () {
   $("#preloader").delay(200).fadeOut("slow");
 });
 
-/*Sticky navbar*/
-window.addEventListener("scroll", function () {
+/*------------------
+      Sticky Navbar
+    --------------------*/
+function navbar() {
   var nav = $("#nav");
   var preH = $("#anchor").offset().top;
 
@@ -16,49 +18,52 @@ window.addEventListener("scroll", function () {
   } else {
     nav.removeClass("nav-outer-sticky");
   }
-});
+}
+/*------------------
+        Parallax
+    --------------------*/
+// var org = $(".bg-text").position().top;
+// function parallax(ele, speed) {
+//   console.log(org);
+//   $(ele)
+//     .first()
+//     .css("top", (org + window.scrollY) * speed);
+// }
 
 /*------------------
         TimeLine
     --------------------*/
-(function ($) {
-  $.fn.timeline = function () {
-    var selectors = {
-      id: $(this),
-      item: $(this).find(".timeline-item"),
-      activeClass: "timeline-item--active",
-      img: ".timeline__img",
-    };
-    selectors.item.eq(0).addClass(selectors.activeClass);
-    selectors.id.css(
-      "background-image",
-      "url(" + selectors.item.first().find(selectors.img).attr("src") + ")"
-    );
-    var itemLength = selectors.item.length;
-    $(window).scroll(function () {
-      var max, min;
-      var pos = $(this).scrollTop();
-      selectors.item.each(function (i) {
-        min = $(this).offset().top - 100;
-        max = $(this).height() + $(this).offset().top;
-        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
-          selectors.item.removeClass(selectors.activeClass);
-          selectors.id.css(
-            "background-image",
-            "url(" + selectors.item.last().find(selectors.img).attr("src") + ")"
-          );
-          selectors.item.last().addClass(selectors.activeClass);
-        } else if (pos <= max - 40 && pos >= min) {
-          selectors.id.css(
-            "background-image",
-            "url(" + $(this).find(selectors.img).attr("src") + ")"
-          );
-          selectors.item.removeClass(selectors.activeClass);
-          $(this).addClass(selectors.activeClass);
-        }
-      });
-    });
-  };
-})(jQuery);
+var selectors = {
+  id: $("#timeline-1"),
+  item: $("#timeline-1").find(".timeline-item"),
+  activeClass: "timeline-item--active",
+  img: ".timeline__img",
+};
 
-$("#timeline-1").timeline();
+function timeline() {
+  var max, min;
+  var pos = window.scrollY;
+  selectors.item.each(function () {
+    if (!$(this).hasClass(selectors.activeClass)) {
+      offset = $(this).offset().top;
+      min = offset - 100;
+      max = $(this).height() + offset - 40;
+      if (pos <= max && pos >= min) {
+        selectors.id.css(
+          "background-image",
+          "url(" + $(this).find(selectors.img).attr("src") + ")"
+        );
+        selectors.item.removeClass(selectors.activeClass);
+        $(this).addClass(selectors.activeClass);
+      }
+    }
+  });
+}
+
+/*------------------
+        On scroll
+    --------------------*/
+window.addEventListener("scroll", function () {
+  navbar();
+  timeline();
+});
